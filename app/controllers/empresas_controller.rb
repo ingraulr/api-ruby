688b2f52e59e629ef6e08d27ad32577d0e204c27
raul <you@example.com>
2018-06-28 16:23:37 -0700

    Empresas crud terminado

--- app/controllers/empresas_controller.rb
+++ app/controllers/empresas_controller.rb
@@ -1,17 +1,33 @@
 class EmpresasController < ApplicationController
+    def index
+      @empresas = Empresa.all
+    end
+
     def new
-        @empresa = Empresa.new(params[:empresa])
-    
+        @empresa = Empresa.new
     end
 
     def create
-        @empresa = Empresa.new(params[:empresa])
+        @empresa = Empresa.new(empresa_params)
     
         @empresa.save
-        redirect_to root_path
+        redirect_to empresas_path
     end
 
     def show
-      
+      @empresa = Empresa.find(params[:id])
     end
+
+    def destroy
+      @empresa = Empresa.find(params[:id])
+      @empresa.destroy
+
+      redirect_to empresas_path
+    end
+
+    private
+
+        def empresa_params
+            params.require(:empresa).permit(:empresa_uid, :empresa_api_key, :ambiente, :objeto, :certificado_pem ,:llave_pem ,:llave_password)
+        end
 end
